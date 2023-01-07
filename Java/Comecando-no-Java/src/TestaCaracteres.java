57f9da8472e032861e25fc2c66e99353069ce6bc
Joao Marcelo-PC <joaomarcelobatistanarciso@gmail.com>
2023-01-07 16:37:47 -0300

    Adicionando novas aulas feitas 05-01e04

---
+++ Java/Comecando-no-Java/src/TestaCaracteres.java
@@ -0,0 +1,21 @@
+
+public class TestaCaracteres {
+	
+	public static void main(String[] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char letraPorValor = 65;
+		System.out.println(letraPorValor);
+		
+		letraPorValor = (char) (letraPorValor + 1);
+		System.out.println(letraPorValor);
+		
+		String palavra = "Alura cursos online de tecnologia";
+		System.out.println(palavra);
+		
+		palavra = palavra + 2020;
+		System.out.println(palavra);
+		
+	}
+}
