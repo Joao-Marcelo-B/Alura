dbc62027bc4ef7c26e5ef8b0b1991097b1a5a5e2
Joao Marcelo-PC <joaomarcelobatistanarciso@gmail.com>
2023-01-07 16:01:40 -0300

    Adicionando arquivos da aula de java 04-07

---
+++ Java/Comecando-no-Java/src/TestaConvesao.java
@@ -0,0 +1,19 @@
+
+public class TestaConvesao {
+	
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		
+		float pontoFlutuante = 3.14f;
+		
+		long numeroGrande = 158495166454L; //64 Bits
+		short valorPequeno = 2131; //16 Bits
+		byte b = 127; //1 Byte 
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1 + valor2;
+		
+		System.out.println(total);
+	}
+}
