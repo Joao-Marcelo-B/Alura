04f69e6a344fb753bf22484282c04c8fca43b69e
Joao Marcelo-PC <joaomarcelobatistanarciso@gmail.com>
2023-01-10 23:26:39 -0300

    Adicionando aula de exercicio

---
+++ Java/Comecando-no-Java/src/NumeroFatorial.java
@@ -0,0 +1,12 @@
+
+public class NumeroFatorial {
+
+	public static void main(String[] args) {
+		
+		int fatorial = 1;
+		for(int n = 2; n <= 10; n++) {
+			fatorial *= n;
+			System.out.println(fatorial);
+		}
+	}
+}
