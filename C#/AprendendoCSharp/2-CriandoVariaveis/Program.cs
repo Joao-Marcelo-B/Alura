20fbf3883ff26cfb659649da94977b88babdcce6
Joao-Marcelo-Not <joaomarcelobatistanarciso@gmail.com>
2023-01-18 10:47:45 -0300

    Adicionando nvovas aulas de C#

---
+++ C#/AprendendoCSharp/2-CriandoVariaveis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        int idade;
+        char nome = 'Ola Mundo';
+        idade = 19;
+        Console.WriteLine(nome);
+    }
+}
