0899675116b22d819fff18f2efaeb09fa4d4e210
Joao-Marcelo-Not <joaomarcelobatistanarciso@gmail.com>
2023-01-23 16:05:02 -0300

    Adicionando novas aulas de C# Orientação a Objeto

--- C#/curso_OrientacaoObjetosC01R-aula03/bytebank/Contas/ContaCorrente.cs
+++ C#/curso_OrientacaoObjetosC01R-aula03/bytebank/Contas/ContaCorrente.cs
@@ -4,16 +4,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace bytebank.Contas
 {
     public class ContaCorrente
     {
-        public int numero_agencia;
-        public string conta;
-        public double saldo = 100;
 
-        public Cliente titular;
+        public static int TotalDeContasCriadas { get; private set; }
+
+        private int numero_agencia;
+        public int Numero_agencia
+        {
+            get { return numero_agencia; }
+            private set 
+            { 
+                if(value > 0)
+                {
+                    this.numero_agencia = value; 
+                }  
+            }
+        }
+
+        public string Conta { get; set; }
+
+        private double saldo = 100;
+
+        public Cliente Titular { get; set; }
 
         public void Depositar(double valor)
         {
@@ -45,6 +62,30 @@ namespace bytebank.Contas
                 destino.Depositar(valor);
                 return true;
             }
+        }
+
+        public void SetSaldo(double valor)
+        {
+            if(valor < 0)
+            {
+                return;
+            }
+            else
+            {
+                this.saldo = valor;
+            }
+        }
+
+        public double GetSaldo()
+        {
+            return this.saldo;
+        }
+
+        public ContaCorrente(int numero_agencia, string numero_conta)
+        {
+            this.Numero_agencia = numero_agencia;
+            this.Conta = numero_conta;
+            TotalDeContasCriadas++;
         }
 
     }
